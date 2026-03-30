60696ace269a61b43c636546ca210eec5762387f
Laurent Morisseau <laurent.morisseau@gmail.com>
2026-03-30 16:52:38 +0200

    feat: automated code generation for Project

---
+++ Models/Project.cs
@@ -0,0 +1,9 @@
+namespace WebApp.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int OrganizationId { get; set; }
+    }
+}
