4a6e3a0da6ae1e443cfa233c8e24b3280465ff56
Laurent Morisseau <laurent.morisseau@gmail.com>
2026-03-31 11:34:37 +0200

    Update Models/User.cs

--- Models/User.cs
+++ Models/User.cs
@@ -1,14 +1,1 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace WebApp.Models
-{
-    public class User
-    {
-        public int Id { get; set; }
-        [Required, EmailAddress]
-        public string Email { get; set; }
-        public string PasswordHash { get; set; }
-        public bool IsSSO { get; set; }
-        public bool EmailConfirmed { get; set; }
-    }
-}
+using System; using System.Collections.Generic; using System.ComponentModel.DataAnnotations; public class User { public int Id { get; set; } [Required] [EmailAddress] public string Email { get; set; } [Required] public string Role { get; set; } public bool IsAccepted { get; set; } }
