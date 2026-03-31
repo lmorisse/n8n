447d267a88c6b0195c13177eccadb26f2a167244
Laurent Morisseau <laurent.morisseau@gmail.com>
2026-03-31 11:35:24 +0200

    Update Models/User.cs

--- Models/User.cs
+++ Models/User.cs
@@ -1,1 +1,1 @@
-using System; using System.Collections.Generic; using System.ComponentModel.DataAnnotations; public class User { public int Id { get; set; } [Required] [EmailAddress] public string Email { get; set; } [Required] public string Role { get; set; } public bool IsAccepted { get; set; } }
+using System; using System.Collections.Generic; namespace YourNamespace.Models { public class User { public int Id { get; set; } public string Email { get; set; } public string Role { get; set; } public bool IsAccepted { get; set; } public ICollection<Project> Projects { get; set; } } }
