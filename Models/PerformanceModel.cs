596865292006c24d7c31b37ff8b2ebe6357b03d4
Laurent Morisseau <laurent.morisseau@gmail.com>
2026-04-01 10:59:16 +0200

    Add PerformanceModel for project KAN-33

---
+++ Models/PerformanceModel.cs
@@ -0,0 +1,11 @@
+namespace ProjectNamespace.Models
+{
+    public class PerformanceModel
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime LastUpdated { get; set; }
+        
+        // Additional properties related to performance, scaling, and reliability
+    }
+}
